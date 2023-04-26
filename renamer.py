ec8ae55289c51e01c12074214dab8d5b67bafe08
GabbasovAlex <shura.gabbasov@mail.ru>
2023-04-26 15:33:02 +0300

    рабочий вариант v1.0

--- renamer.py
+++ renamer.py
@@ -1,12 +1,25 @@
 import os
+import cv2
 import shutil
+from PIL import Image, ImageDraw, ImageFont
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 
 # Путь к общей папке, где находятся все папки с моделями
-root_dir = "C:/Users/shura/Desktop/test"
+root_dir = "C:/Users/shura/Desktop/models"
 
 # Папки, которые содержат изображения и видео
 pics_dir = "Pics"
 vids_dir = "Vids"
+
+# Удаляем файлы формата .txt и .pdf
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    for file in filenames:
+        file_path = os.path.join(dirpath, file)
+        file_extension = os.path.splitext(file_path)[1]
+        if file_extension.lower() in [".txt", ".pdf"]:
+            os.remove(file_path)
 
 # Переименовываем файлы в каждой папке
 for model_dir in os.listdir(root_dir):
@@ -26,11 +39,29 @@ for model_dir in os.listdir(root_dir):
             for file in filenames:
                 file_path = os.path.join(dirpath, file)
                 file_extension = os.path.splitext(file_path)[1]
+
+                #Photos
                 if file_extension.lower() in [".jpg", ".png",".jpeg", ".bmp"]:
                     # Переименовываем файлы в папке Pics
                     new_file_name = f"Image uploaded by Vaso de Sangre - Join TG @vasodesangre {pic_num}{file_extension}"
-                    shutil.move(file_path, os.path.join(model_dir_path, pics_dir, new_file_name))
+                    # Открываем изображение
+                    img = Image.open(file_path)
+                    # Получаем размер изображения
+                    width, height = img.size
+                    # Создаем объект ImageDraw и объект шрифта
+                    draw = ImageDraw.Draw(img)
+                    font = ImageFont.truetype("MYRIADPRO-REGULAR.OTF", size=int(((height*width//1600)**0.5)))
+                    # Вычисляем координаты для левого нижнего угла текста
+                    text_width, text_height = draw.textsize(new_file_name, font)
+                    x = 10
+                    y = height - text_height - 10
+                    # Рисуем текстовую вотермарку в левом нижнем углу изображения
+                    draw.text((x, y), "t.me/vasodesangre", font=font, fill=(190, 190, 195, 255), outline=(0, 0, 0))
+                    # Сохраняем изображение с вотермаркой в папке Pics
+                    img.save(os.path.join(model_dir_path, pics_dir, new_file_name))
                     pic_num += 1
+
+                #Videos
                 elif file_extension.lower() in [".mp4", ".mov", ".m4v", ".ts", ".gif"]:
                     # Переименовываем файлы в папке Vids
                     new_file_name = f"Video uploaded by Vaso de Sangre - Join TG @vasodesangre {vid_num}{file_extension}"
