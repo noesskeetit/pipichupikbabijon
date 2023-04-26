0e694a8d6cb32056df15069d1638a8af627cf67b
GabbasovAlex <shura.gabbasov@mail.ru>
2023-04-26 22:45:12 +0300

    рабочий вариант v5.0

--- renamer.py
+++ renamer.py
@@ -3,6 +3,7 @@ import shutil
 from PIL import ImageDraw, ImageFont
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+from tqdm import tqdm
 from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -15,14 +16,14 @@ processed_folders_file = "processed.txt"
 pics_dir = "Pics"
 vids_dir = "Vids"
 
-# Удаляем файлы формата .txt и .pdf
+# Удаляем файлы формата .txt и .pdf and so on
 for dirpath, dirnames, filenames in os.walk(root_dir):
     for file in filenames:
         file_path = os.path.join(dirpath, file)
         file_extension = os.path.splitext(file_path)[1]
-        if file_extension.lower() in [".txt", ".pdf", ".gif"]:
+        if file_extension.lower() in [".txt", ".pdf", ".gif", ".jfif", ".json", ".html", ".js", ".css"]:
             os.remove(file_path)
-        if str(file) == "@nsfwcherry [TG].png":
+        if str(file) == "@nsfwcherry [TG].png" or "thumb" in str(file):
             os.remove(file_path)
 
 # Переименовываем файлы в каждой папке
@@ -44,43 +45,54 @@ for model_dir in os.listdir(root_dir):
         vid_num = 1
 
         for dirpath, dirnames, filenames in os.walk(model_dir_path):
-            for file in filenames:
-                file_path = os.path.join(dirpath, file)
-                file_extension = os.path.splitext(file_path)[1]
+            for file in tqdm(filenames):
+                try:
+                    file_path = os.path.join(dirpath, file)
+                    file_extension = os.path.splitext(file_path)[1]
+                    #Photos
+                    if file_extension.lower() in [".jpg", ".png",".jpeg", ".bmp"]:
+                        # Переименовываем файлы в папке Pics
+                        new_file_name = f"Image uploaded by Vaso de Sangre - Join TG @vasodesangre {pic_num}{file_extension}"
+                        # Открываем изображение
+                        img = Image.open(file_path)
+                        # Получаем размер изображения
+                        width, height = img.size
+                        # Создаем объект ImageDraw и объект шрифта
+                        draw = ImageDraw.Draw(img)
+                        font = ImageFont.truetype("MYRIADPRO-REGULAR.OTF", size=int(((height*width//1600)**0.5)))
+                        # Вычисляем координаты для левого нижнего угла текста
+                        text_width, text_height = draw.textsize("t.me/vasodesangre", font)
+                        x = 10
+                        y = height - text_height - 10
+                        # Рисуем текстовую вотермарку в левом нижнем углу изображения
+                        draw.text((x, y), "t.me/vasodesangre", font=font, fill=(190, 190, 195, 255), outline=(0, 0, 0))
+                        # Сохраняем изображение с вотермаркой в папке Pics
+                        # img.save(os.path.join(model_dir_path, pics_dir, new_file_name))
+                        # Удаляем оригинальное изображение
+                        os.remove(file_path)
+                        # Сохраняем изображение с вотермаркой под тем же именем и в той же папке
+                        img.save(os.path.join(model_dir_path, pics_dir, new_file_name))
+                        pic_num += 1
+                    #Videos
+                    elif file_extension.lower() in [".mp4", ".mov", ".m4v", ".ts", ".mkv"]:
+                        # Переименовываем файлы в папке Vids
+                        new_file_name = f"Video uploaded by Vaso de Sangre - Join TG @vasodesangre {vid_num}{file_extension}"
+                        shutil.move(file_path, os.path.join(model_dir_path, vids_dir, new_file_name))
+                        vid_num += 1
+                except OSError as e:
+                    print(f"Removing file {file}, error: {e}")
+                    os.remove(os.path.join(dirpath, file))
+                    continue
+                except TypeError as t:
+                    print(f"Removing file {file}, error: {t}")
+                    os.remove(os.path.join(dirpath, file))
+                    continue
+                except ValueError as v:
+                    print(f"Removing file {file}, error: {v}")
+                    os.remove(os.path.join(dirpath, file))
+                    continue
 
-                #Photos
-                if file_extension.lower() in [".jpg", ".png",".jpeg", ".bmp"]:
-                    # Переименовываем файлы в папке Pics
-                    new_file_name = f"Image uploaded by Vaso de Sangre - Join TG @vasodesangre {pic_num}{file_extension}"
-                    # Открываем изображение
-                    img = Image.open(file_path)
-                    # Получаем размер изображения
-                    width, height = img.size
-                    # Создаем объект ImageDraw и объект шрифта
-                    draw = ImageDraw.Draw(img)
-                    font = ImageFont.truetype("MYRIADPRO-REGULAR.OTF", size=int(((height*width//1600)**0.5)))
-                    # Вычисляем координаты для левого нижнего угла текста
-                    text_width, text_height = draw.textsize("t.me/vasodesangre", font)
-                    x = 10
-                    y = height - text_height - 10
-                    # Рисуем текстовую вотермарку в левом нижнем углу изображения
-                    draw.text((x, y), "t.me/vasodesangre", font=font, fill=(190, 190, 195, 255), outline=(0, 0, 0))
-                    # Сохраняем изображение с вотермаркой в папке Pics
 
-                    # img.save(os.path.join(model_dir_path, pics_dir, new_file_name))
-
-                    # Удаляем оригинальное изображение
-                    os.remove(file_path)
-                    # Сохраняем изображение с вотермаркой под тем же именем и в той же папке
-                    img.save(os.path.join(model_dir_path, pics_dir, new_file_name))
-                    pic_num += 1
-
-                #Videos
-                elif file_extension.lower() in [".mp4", ".mov", ".m4v", ".ts", ".mkv"]:
-                    # Переименовываем файлы в папке Vids
-                    new_file_name = f"Video uploaded by Vaso de Sangre - Join TG @vasodesangre {vid_num}{file_extension}"
-                    shutil.move(file_path, os.path.join(model_dir_path, vids_dir, new_file_name))
-                    vid_num += 1
 
     with open(processed_folders_file, 'a') as f:
         f.write(model_dir + '\n')
@@ -92,4 +104,4 @@ for model_dir in os.listdir(root_dir):
                 if not os.listdir(folder):
                     os.rmdir(folder)
 
-print("The renaming process is done. Enjoy <3 :)")
+print("The renaming and watermarking process is done. Enjoy <3 :)")
